68dc183aace46e1253ebb470ca8e21ac2d0b4816
tomi1108 <h.tomimori.290@ms.saitama-u.ac.com>
2024-08-01 23:47:55 +0900

    [add] 評価結果をcommit

--- shell/run_sl.sh
+++ shell/run_sl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# anaconda_env=openpcdet
-anaconda_env=faiss
+anaconda_env=openpcdet
+# anaconda_env=faiss
 src_path=../src/
 dataset_path=../dataset/
 results_path=../results/
@@ -10,7 +10,7 @@ dataset_type=cifar10
 server_file_name=server.py
 client_file_name=client.py
 
-port_number=3333
+port_number=2222
 seed=42
 num_clients=2
 num_rounds=25
@@ -21,7 +21,7 @@ momentum=0.9
 weight_decay=0.0001
 temperature=0.07
 output_size=64
-data_partitions=(4 5) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
+data_partitions=(5) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
 fed_flag=False
 proto_flag=False
 self_kd_flag=False
