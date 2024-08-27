f60f5277b7958f15c59e30e629e27f342b141b16
tomi1108 <h.tomimori.290@ms.saitama-u.ac.com>
2024-08-28 02:21:44 +0900

    [add] Proto_SFLの評価結果

--- shell/run_sl.sh
+++ shell/run_sl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# anaconda_env=openpcdet
-anaconda_env=faiss
+anaconda_env=openpcdet
+# anaconda_env=faiss
 src_path=../src/
 dataset_path=../dataset/
 results_path=../results/
@@ -20,7 +20,7 @@ learning_rate=0.01
 momentum=0.9
 weight_decay=0.0001
 temperature=0.07
-data_partitions=(0) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
+data_partitions=(4 5) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
 
 fed_flag=True
 
