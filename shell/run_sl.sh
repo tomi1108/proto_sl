471dbc478dc559ff206a4cc8e4f09fad857f5a6c
tomi1108 <h.tomimori.290@ms.saitama-u.ac.com>
2024-08-27 22:28:52 +0900

    [add] 評価結果

--- shell/run_sl.sh
+++ shell/run_sl.sh
@@ -10,7 +10,7 @@ dataset_type=cifar10
 server_file_name=server.py
 client_file_name=client.py
 
-port_number=3333
+port_number=1111
 seed=42
 num_clients=2
 num_rounds=25
@@ -20,20 +20,20 @@ learning_rate=0.01
 momentum=0.9
 weight_decay=0.0001
 temperature=0.07
-data_partitions=(1 4) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
+data_partitions=(0 1 4 5) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
 
-fed_flag=True
+fed_flag=False
 
 proto_flag=False
 queue_size=16384
 output_size=64
 
-con_flag=True # モデル対照学習を使用
+con_flag=False # モデル対照学習を使用
 
 self_kd_flag=False
 
 current_date=$(date +%Y-%m-%d)
-save_data=False
+save_data=True
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate ${anaconda_env}
