9b4a90710b6f2dd26f312f9e7f922fbaf2018f25
tomi1108 <h.tomimori.290@ms.saitama-u.ac.com>
2024-08-28 15:01:20 +0900

    [add] Proto_SFLの評価結果

--- shell/run_sl.sh
+++ shell/run_sl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# anaconda_env=openpcdet
-anaconda_env=faiss
+anaconda_env=openpcdet
+# anaconda_env=faiss
 src_path=../src/
 dataset_path=../dataset/
 results_path=../results/
@@ -10,19 +10,19 @@ dataset_type=cifar10
 server_file_name=server.py
 client_file_name=client.py
 
-port_number=1111
+port_number=3333
 seed=42
 num_clients=2
-num_rounds=50
-num_epochs=5
+num_rounds=25
+num_epochs=10
 batch_sizes=(128)
 learning_rate=0.01
 momentum=0.9
 weight_decay=0.0001
 temperature=0.07
-data_partitions=(0 1 4 5) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
+data_partitions=(0 1) # 0: IID, 1: Non-IID(class), 2: Non-IID(Dirichlet(0.6)), 3: Non-IID(Dirichlet(0.3)) 4: Non-IID(Dirichlet(0.1)), 5: Non-IID(Dirichlet(0.05))
 
-fed_flag=False
+fed_flag=True
 
 proto_flag=True
 queue_size=16384
