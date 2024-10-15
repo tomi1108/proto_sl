fa7b183035364a12a4476ab4793f6d32ff167967
tomi1108 <h.tomimori.290@ms.saitama-u.ac.com>
2024-10-15 16:59:31 +0900

    [other] git pull用のcommit

--- shell/run_sl.sh
+++ shell/run_sl.sh
@@ -11,10 +11,10 @@ client_file_name=client.py
 
 port_number=5555
 seed=42
-num_clients=2
+num_clients=5
 num_rounds=50
 num_epochs=5
-batch_sizes=(4)
+batch_sizes=(64)
 learning_rate=0.01
 momentum=0.9
 weight_decay=0.0001
